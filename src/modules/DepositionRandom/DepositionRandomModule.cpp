339cdadd195aaa1c320a771ad8bc7ab339724548
Solvalou <schm.seb@googlemail.com>
2018-04-01 00:00:48 +0200

    Added DepositionRandom

---
+++ src/modules/DepositionRandom/DepositionRandomModule.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * @brief Implementation of [DepositionRandom] module
+ * @copyright Copyright (c) 2018 CERN and the Allpix Squared authors.
+ * This software is distributed under the terms of the MIT License, copied verbatim in the file "LICENSE.md".
+ * In applying this license, CERN does not waive the privileges and immunities granted to it by virtue of its status as an
+ * Intergovernmental Organization or submit itself to any jurisdiction.
+ */
+
+#include "DepositionRandomModule.hpp"
+
+#include <string>
+#include <utility>
+
+#include "core/utils/log.h"
+
+using namespace allpix;
+
+DepositionRandomModule::DepositionRandomModule(Configuration config, Messenger* messenger, GeometryManager* geo_manager)
+    : Module(std::move(config)), geo_manager_(geo_manager), messenger_(messenger) {
+
+    // ... Implement ... (Typically bounds the required messages and optionally sets configuration defaults)
+    // Input required by this module
+    messenger_->bindMulti(this, &DepositionRandomModule::messages_, MsgFlags::REQUIRED);
+}
+
+void DepositionRandomModule::init() {
+    // Loop over detectors and do something
+    std::vector<std::shared_ptr<Detector>> detectors = geo_manager_->getDetectors();
+    for(auto& detector : detectors) {
+        // Get the detector name
+        std::string detectorName = detector->getName();
+        LOG(DEBUG) << "Detector with name " << detectorName;
+    }
+}
+
+void DepositionRandomModule::run(unsigned int) {
+    // ... Implement ... (Typically uses the configuration to execute function and outputs an message)
+    // Loop through all receieved messages and print some information
+    for(auto& message : messages_) {
+        std::string detectorName = message->getDetector()->getName();
+        LOG(DEBUG) << "Picked up " << message->getData().size() << " objects from detector " << detectorName;
+    }
+}
+
